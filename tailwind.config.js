18f474179f8715ab8ee26395b0c46b7cc389669b
Jonathan Morales <jonmor@stanford.edu>
2021-10-31 17:28:11 -0700

    loading Dosis font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {},
+    fontFamily: {
+      custom: ["Dosis", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
